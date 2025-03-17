31fc031c04ca05a8c4d270be743edab98abdb2e8
dhruvsharma123 <dhruv1931.be21@chitkara.edu.in>
2025-03-17 13:50:29 +0530

    Dhruv Sharma Add:smart home devices

---
+++ SmartHomeDevices.java
@@ -0,0 +1,35 @@
+class Device{
+    String deviceId;
+    String status;
+
+    Device(String deviceId, String status){
+        this.deviceId = deviceId;
+        this.status = status;
+    }
+
+    public void displayStatus() {
+        System.out.println("Device ID: " + deviceId);
+        System.out.println("Status: " + status);
+    }
+}
+
+
+class Thermostat extends Device {
+    double temperatureSetting;
+
+    public Thermostat(String deviceId, String status, double temperatureSetting) {
+        super(deviceId, status);
+        this.temperatureSetting = temperatureSetting;
+    }
+
+    public void displayStatus() {
+        super.displayStatus();
+        System.out.println("Temperature Setting: " + temperatureSetting + "°C");
+    }
+}
+public class SmartHomeDevices {
+    public static void main(String[] args) {
+        Thermostat thermostat = new Thermostat("TH123", "On", 22.5);
+        thermostat.displayStatus();
+    }
+}
